2d991536eaf91daf17389accdcdc6333e8c7e5be
FairgonnDev <alejandro.bejarano@estudiant.enti.cat>
2026-06-13 01:38:16 +0200

    SendP2PJoin

--- Server/include/database/DatabaseConfig.h
+++ Server/include/database/DatabaseConfig.h
@@ -3,6 +3,7 @@
 // Database connection settings.
 inline constexpr const char* DATABASE_SERVER = "127.0.0.1::3360";
 inline constexpr const char* DATABASE_USERNAME = "root";
-inline constexpr const char* DATABASE_PASSWORD = "";
+//inline constexpr const char* DATABASE_PASSWORD = "";
+inline constexpr const char* DATABASE_PASSWORD = "redes";
 //inline constexpr const char* DATABASE_PASSWORD = "enti";
-inline constexpr const char* DATABASE_NAME = "3raya";
+inline constexpr const char* DATABASE_NAME = "tresenraya";
